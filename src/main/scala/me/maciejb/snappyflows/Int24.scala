90ff190ce7bc2974ffaa1c1de3fa485f0a788ae7
Maciej Bilas <maciej.bilas@gmail.com>
2015-12-04 19:47:23 +0100

    Checksums

---
+++ src/main/scala/me/maciejb/snappyflows/Int24.scala
@@ -0,0 +1,17 @@
+package me.maciejb.snappyflows
+
+import akka.stream.io.ByteStringParser.ByteReader
+import akka.util.ByteString
+
+
+private[snappyflows] object Int24 {
+
+  def readLE(r: ByteReader): Int = r.readByte() | r.readByte() << 8 | r.readByte() << 16
+
+  def writeLE(number: Int): ByteString = ByteString.apply(
+    (number & 0xff).toByte,
+    (number >> 8 & 0xff).toByte,
+    (number >> 16 & 0xff).toByte
+  )
+
+}
--- src/main/scala/me/maciejb/snappyflows/SnappyFlows.scala
+++ src/main/scala/me/maciejb/snappyflows/SnappyFlows.scala
@@ -7,17 +7,26 @@ import akka.stream.io.ByteStringParser
 import akka.stream.io.ByteStringParser.{ByteReader, ParseStep}
 import akka.stream.scaladsl.Flow
 import akka.util.ByteString
-import org.xerial.snappy.Snappy
+import org.xerial.snappy.{PureJavaCrc32C, Snappy}
 
 import scala.util.control.NoStackTrace
 
+class InvalidChecksum(expected: Int, actual: Int) extends Exception
+
 object SnappyFlows {
+
+  private implicit val byteOrder = ByteOrder.LITTLE_ENDIAN
+
+  val MaxChunkSize = 65536
+  val DefaultChunkSize = MaxChunkSize
 
   def decompress(verifyChecksums: Boolean = false): Flow[ByteString, ByteString, Unit] =
     Flow.fromGraph(new Decompressor(verifyChecksums))
 
   private class Decompressor(verifyChecksums: Boolean) extends ByteStringParser[ByteString] {
     override def createLogic(inheritedAttributes: Attributes) = new ParsingLogic {
+
+      val crc32c = new PureJavaCrc32C
 
       object HeaderParse extends ParseStep[ByteString] {
         override def parse(reader: ByteReader): (ByteString, ParseStep[ByteString]) = {
@@ -28,22 +37,28 @@ object SnappyFlows {
       }
 
       object ChunkParser extends ParseStep[ByteString] {
-        def checksumed(checksum: Int)(chunk: => ByteString) = {
-          chunk
+        def withChecksumChecked(storedChecksum: Int)(chunk: => ByteString) = {
+          if (verifyChecksums) {
+            val actual = SnappyChecksum.checksum(chunk)
+            if (actual == storedChecksum) chunk
+            else throw new InvalidChecksum(storedChecksum, actual)
+          } else {
+            chunk
+          }
         }
 
         override def parse(reader: ByteReader) = {
           reader.readByte() match {
             case SnappyFramed.Flags.CompressedData =>
               val segmentLength = Int24.readLE(reader) - 4
-              val uncompressed = checksumed(reader.readIntLE()) {
+              val uncompressed = withChecksumChecked(reader.readIntLE()) {
                 val segment = reader.take(segmentLength)
                 ByteString(Snappy.uncompress(segment.toArray))
               }
               (uncompressed, ChunkParser)
             case SnappyFramed.Flags.UncompressedData =>
               val segmentLength = Int24.readLE(reader) - 4
-              val chunk = checksumed(reader.readIntLE()) {reader.take(segmentLength)}
+              val chunk = withChecksumChecked(reader.readIntLE()) {reader.take(segmentLength)}
               (chunk, ChunkParser)
             case _ => sys.error("Illegal chunk flag")
           }
@@ -54,8 +69,10 @@ object SnappyFlows {
     }
   }
 
-  def compress(chunkSize: Int = 65536): Flow[ByteString, ByteString, Unit] =
+  def compress(chunkSize: Int = 65536): Flow[ByteString, ByteString, Unit] = {
+    require(chunkSize < MaxChunkSize, s"Chunk size $chunkSize exceeded max chunk size of $MaxChunkSize.")
     Flow.fromGraph(new Compressor(chunkSize))
+  }
 
   private class Compressor(chunkSize: Int) extends ByteStringParser[ByteString] {
     override def createLogic(inheritedAttributes: Attributes) = new ParsingLogic {
@@ -73,9 +90,10 @@ object SnappyFlows {
             case e: Exception with NoStackTrace => reader.takeAll()
           }
           val compressed = Snappy.compress(chunk.toArray)
+          val checksum = SnappyChecksum.checksum(chunk)
           val result = ByteString.newBuilder
             .append(Int24.writeLE(compressed.length))
-            .putInt(0xBEEF)(ByteOrder.LITTLE_ENDIAN)
+            .putInt(checksum)
             .putBytes(compressed)
             .result()
           (result, ChunkParser)
@@ -88,30 +106,26 @@ object SnappyFlows {
 
 }
 
-private[snappyflows] object Int24 {
-  def readLE(r: ByteReader): Int = {
-    r.readByte() | r.readByte() << 8 | r.readByte() << 16
-  }
-
-  def writeLE(number: Int): ByteString = {
-    ByteString.apply(
-      (number & 0xff).toByte,
-      (number >> 8 & 0xff).toByte,
-      (number >> 16 & 0xff).toByte
-    )
-  }
-}
-
 object SnappyFramed {
   object Flags {
     val StreamIdentifier = 0xff
     val UncompressedData = 0x01
     val CompressedData = 0x00
-
   }
 
-  private val HeaderBytes = Array[Byte](
-    Flags.StreamIdentifier.toByte, 0x06, 0x00, 0x00, 0x73, 0x4e, 0x61, 0x50, 0x70, 0x59)
+  private val HeaderBytes =
+    Array[Byte](Flags.StreamIdentifier.toByte, 0x06, 0x00, 0x00, 0x73, 0x4e, 0x61, 0x50, 0x70, 0x59)
 
   val Header = ByteString(HeaderBytes)
+}
+
+object SnappyChecksum {
+  val MaskDelta = 0xa282ead8
+
+  def checksum(data: ByteString): Int = {
+    val crc32c = new PureJavaCrc32C
+    crc32c.update(data.toArray, 0, data.length)
+    val crc = crc32c.getIntegerValue
+    ((crc >>> 15) | (crc << 17)) + MaskDelta
+  }
 }
