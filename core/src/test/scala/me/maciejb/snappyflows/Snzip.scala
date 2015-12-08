a5d7016ad62bc8d2737d0f5121134f84929010f1
Maciej Bilas <maciej.bilas@gmail.com>
2015-12-08 10:23:09 +0100

    Snzip utility

---
+++ core/src/test/scala/me/maciejb/snappyflows/Snzip.scala
@@ -0,0 +1,28 @@
+package me.maciejb.snappyflows
+
+import java.io.File
+
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{Keep, Sink, Source}
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
+
+object Snzip extends App {
+
+  implicit val system = ActorSystem()
+  implicit val mat = ActorMaterializer()
+  implicit val ec = concurrent.ExecutionContext.Implicits.global
+
+  val source = Source.file(new File(args(0)), chunkSize = 65536)
+
+  val sink = Sink.file(new File(args(0) + ".sz"))
+
+  Await.ready(
+    source.via(SnappyFlows.compress()).log("compress").toMat(sink)(Keep.right).run()
+      .andThen { case _ => system.terminate() },
+    1.minutes)
+
+}
