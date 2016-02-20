7fc010d2f66723f4ee7e29f9583133c3826bb7ca
Maciej Bilas <maciej.bilas@gmail.com>
2016-02-20 14:10:46 +0100

    ChunkingTest: increase patience for async tests

--- core/src/test/scala/me/maciejb/snappyflows/impl/ChunkingTest.scala
+++ core/src/test/scala/me/maciejb/snappyflows/impl/ChunkingTest.scala
@@ -2,18 +2,21 @@ package me.maciejb.snappyflows.impl
 
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.Sink
-import akka.stream.scaladsl.{Keep, Source}
+import akka.stream.scaladsl.{Keep, Sink, Source}
 import akka.testkit.TestKit
 import akka.util.ByteString
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.{BeforeAndAfterAll, FlatSpec, Matchers}
+
+import scala.concurrent.duration._
 
 
 class ChunkingTest extends FlatSpec with Matchers with BeforeAndAfterAll with ScalaFutures {
 
   implicit lazy val system = ActorSystem()
   implicit lazy val mat = ActorMaterializer()
+
+  override implicit val patienceConfig = PatienceConfig(scaled(300.millis), scaled(15.millis))
 
   it should "split a ByteString larger than the chunkSize into chunks" in {
     val fut = Source.single(ByteString.fromString("foobar"))
