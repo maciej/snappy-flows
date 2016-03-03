5f60b6cf6452f27dc6f91a3309ea02b00b01a274
Maciej Bilas <maciej.bilas@gmail.com>
2016-03-03 13:10:50 +0100

    Improve ChunkingTests

--- core/src/test/scala/me/maciejb/snappyflows/impl/ChunkingTest.scala
+++ core/src/test/scala/me/maciejb/snappyflows/impl/ChunkingTest.scala
@@ -8,8 +8,9 @@ import akka.util.ByteString
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.{BeforeAndAfterAll, FlatSpec, Matchers}
 
+import scala.collection.immutable
+import scala.concurrent.Future
 import scala.concurrent.duration._
-
 
 class ChunkingTest extends FlatSpec with Matchers with BeforeAndAfterAll with ScalaFutures {
 
@@ -18,34 +19,41 @@ class ChunkingTest extends FlatSpec with Matchers with BeforeAndAfterAll with Sc
 
   override implicit val patienceConfig = PatienceConfig(scaled(300.millis), scaled(15.millis))
 
-  it should "split a ByteString larger than the chunkSize into chunks" in {
-    val fut = Source.single(ByteString.fromString("foobar"))
-      .via(Chunking.fixedSize(3))
-      .grouped(10)
-      .toMat(Sink.head)(Keep.right).run()
+  def byteStringSeq(strings: String*) = strings.map(ByteString.fromString).to[immutable.Seq]
 
-    whenReady(fut) { chunks =>
-      chunks shouldEqual Seq(
-        ByteString.fromString("foo"),
-        ByteString.fromString("bar")
-      )
-    }
+  def chunk(size: Int, elems: immutable.Seq[ByteString]): Future[immutable.Seq[ByteString]] = {
+    Source(elems)
+      .via(Chunking.fixedSize(size))
+      .grouped(1024)
+      .toMat(Sink.head)(Keep.right).run()
+  }
+
+  it should "split a ByteString larger than the chunkSize into chunks" in {
+    whenReady(chunk(3, byteStringSeq("foobar"))) {_ shouldEqual byteStringSeq("foo", "bar")}
   }
 
   it should "always emit the last element on upstream termination" in {
-    import collection.immutable
-    val fut = Source(immutable.Seq("abc", "bcd"))
-      .map(ByteString.fromString)
-      .via(Chunking.fixedSize(4))
-      .grouped(10)
-      .toMat(Sink.head)(Keep.right).run()
+    whenReady(chunk(4, byteStringSeq("abc", "bcd"))) {_ shouldEqual byteStringSeq("abcb", "cd")}
+  }
 
-    whenReady(fut) { chunks =>
-      chunks shouldEqual Seq(
-        ByteString.fromString("abcb"),
-        ByteString.fromString("cd")
-      )
+  it should "emit all small trailing chunks" in {
+    whenReady(chunk(1, byteStringSeq("abcde"))) {_ shouldEqual byteStringSeq("a", "b", "c", "d", "e")}
+  }
+
+  it should "emit single element on completion even if smaller than chunk size" in {
+    whenReady(chunk(42, byteStringSeq("abcd"))) {_ shouldEqual byteStringSeq("abcd")}
+  }
+
+  it should "complete successfully if input is empty" in {
+    val fut = Source.empty.via(Chunking.fixedSize(42)).toMat(Sink.headOption)(Keep.right).run()
+    whenReady(fut) {
+      case _: Some[ByteString] => fail("Not expecting any elements here")
+      case None => /* all fine */
     }
+  }
+
+  it should "fail if chunk size is 0" in {
+    an[IllegalArgumentException] should be thrownBy {Chunking.fixedSize(0)}
   }
 
   override protected def afterAll() = {
